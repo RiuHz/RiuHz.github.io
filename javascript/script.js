2492ee9b0b03a78ffe16795bcfeccf2b5c193104
Gaetano Di Tota <ditotagaetano@outlook.it>
2026-03-30 06:34:32 +0200

    Ristrutturato il layout e lo stile del sito, rimuovendo sezioni non necessarie e semplificando il CSS per una migliore responsività

--- javascript/script.js
+++ javascript/script.js
@@ -6,16 +6,4 @@ function setSystemTheme() {
     document.documentElement.setAttribute('data-theme', systemPreference);
 }
 
-function setupThemeButton() {
-    const themeToggleButton = document.getElementById('theme-button');
-
-    themeToggleButton.addEventListener('click', () => {
-        const currentTheme = document.documentElement.getAttribute('data-theme');
-        const newTheme = (currentTheme === 'light') ? 'dark' : 'light';
-
-        document.documentElement.setAttribute('data-theme', newTheme);
-    });
-}
-
 setSystemTheme();
-setupThemeButton();
