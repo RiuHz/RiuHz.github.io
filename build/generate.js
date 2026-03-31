0b9581cd5fe3701ec98db6f8c7e6bc5d4565fc49
Gaetano Di Tota <ditotagaetano@outlook.it>
2026-03-31 22:48:34 +0200

    Added generate.js and projects.json from automatic build

---
+++ build/generate.js
@@ -0,0 +1,37 @@
+"use strict";
+
+import { readFileSync, writeFileSync } from "fs";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+function escapeHTML(string) {
+    return string
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;");
+}
+
+const projects = JSON.parse(
+    readFileSync(join(__dirname, "build/projects.json"), "utf-8")
+);
+
+const projectsHTML = projects.map(project => `
+    <section class="project">
+        <h3>${escapeHTML(project.title)}</h3>
+        <span>${project.languages.map(escapeHTML).join(" ")}</span>
+        <p>${escapeHTML(project.origin)}</p>
+        <p>${escapeHTML(project.description)}</p>
+    </section>
+`).join("\n");
+
+const template = readFileSync("index.html", "utf-8");
+
+const output = template.replace(
+    "<!-- PROJECTS -->",
+    projectsHTML
+);
+
+writeFileSync("dist/index.html", output);
